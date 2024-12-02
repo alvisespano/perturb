3462b076498577dcaf3a8a99215b862b5dcddb9d
Alvise Spanò <spano.alvise@gmail.com>
2024-12-02 11:59:27 +0100

    Update rectangle.py

--- dataset/python/rectangle.py
+++ dataset/python/rectangle.py
@@ -1,3 +1,4 @@
+
 class Rectangle:
     def __init__(self, width, height):
         self.width = width
@@ -14,29 +15,38 @@ class Rectangle:
             return self.area() == other.area()
         return False
 
-    def __str__(self):
-        return f"Rectangle(width={self.width}, height={self.height})"
 
 class Square(Rectangle):
-    def __init__(self, side_length):
-        super().__init__(side_length, side_length)
+    def __init__(self, side):
+        super().__init__(side, side)
 
-    def __str__(self):
-        return f"Square(side_length={self.width})"
+
+class ParaSquare(Square):
+    def __init__(self, side):
+        super().__init__(side)
+
+    def area(self):
+        return self.width ** 2
+
+
+class Rectangle__cp:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.tmp = width
+
+    def area(self):
+        return self.tmp * self.height
+
+    def perimeter(self):
+        return 2 * (self.tmp + self.height)
+
+
+    
+
 
 def test():
-    rectangle1 = Rectangle(5, 10)
-    rectangle2 = Rectangle(2, 25)
+    a = Rectangle(5, 10)
+    b = ParaSquare(25)
 
-    print(rectangle1)
-    print("Area:", rectangle1.area())
-    print("Perimeter:", rectangle1.perimeter())
-
-    print(rectangle2)
-    print("Area:", rectangle2.area())
-    print("Perimeter:", rectangle2.perimeter())
-
-    if rectangle1 == rectangle2:
-        print("The two rectangles have the same area.")
-    else:
-        print("The two rectangles have different areas.")
+    return a == b

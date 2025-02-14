ef546507a604c2a983807234f52d5fa749245710
Alvise Spanò <spano.alvise@gmail.com>
2025-02-14 12:35:32 +0100

    Update find_max_count.py

--- dataset/python/notorious/find_max_count.py
+++ dataset/python/notorious/find_max_count.py
@@ -13,6 +13,22 @@ def find_max_count(A):
     return most_frequent
 
 
+def find_max_count__bug(A):
+    length = len(A) 
+    max_frequency = 0
+    most_frequent = 0
+    for i in range(length):
+        count = 0
+        for j in range(i, length):
+            if A[i] == A[j]:
+                count += 1
+        if count > max_frequency:
+            max_frequency = count
+            most_frequent = A[i]
+    return most_frequent
+
+
+
 def find_max_count__cp(A):  
     length = len(A) 
     max_frequency = 0
@@ -23,6 +39,24 @@ def find_max_count__cp(A):
         U = A
         for j in range(i + 1, length):
             k = j + 1
+            if U[i] == A[k - 1]:
+                count += 1
+        if count > max_frequency:
+            max_frequency = count
+            most_frequent = U[i]
+            tmp = most_frequent
+    return tmp
+
+def find_max_count__cp__bug(A):  
+    length = len(A) 
+    max_frequency = 0
+    most_frequent = 0
+    tmp = most_frequent
+    for i in range(length):
+        count = 0
+        U = A
+        for j in range(i + 1, length):
+            k = j
             if U[i] == A[k - 1]:
                 count += 1
         if count > max_frequency:
@@ -47,6 +81,22 @@ def find_max_count__cf(A):
             most_frequent = A[i]
     return most_frequent
 
+def find_max_count__cf__bug(A):  
+    length = len(A) 
+    foo = 2
+    max_frequency = 0
+    most_frequent = foo - foo
+    for i in range(length):
+        count = 0
+        for j in range(i + foo, length):
+            if A[i] == A[j]:
+                count += foo
+        if count > max_frequency:
+            max_frequency = count
+            most_frequent = A[i]
+    return most_frequent
+
+
 
 def find_max_count__cp__cf(A):  
     length = len(A) 
@@ -60,6 +110,25 @@ def find_max_count__cp__cf(A):
         for j in range(i + foo, length):
             k = j + 1
             if A[i] == U[k - 1]:
+                count += foo
+        if count > max_frequency:
+            max_frequency = count
+            most_frequent = U[i]
+            tmp = most_frequent
+    return tmp
+
+def find_max_count__cp__cf__bug(A):  
+    length = len(A) 
+    foo = 1
+    max_frequency = foo
+    most_frequent = foo - foo
+    tmp = most_frequent
+    for i in range(length):
+        count = 0
+        U = A
+        for j in range(i + foo, length):
+            k = j + 1
+            if A[i] == U[k]:
                 count += foo
         if count > max_frequency:
             max_frequency = count
